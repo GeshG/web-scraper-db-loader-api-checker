330951b4acc95770fe3b6e406e3cbe16c0d5d9eb
GeshG <georgi.ginchev@gmail.com>
2022-11-25 18:57:27 +0200

    Add files via upload

--- fastapi_checker/crud.py
+++ fastapi_checker/crud.py
@@ -1,14 +1,11 @@
 from sqlalchemy.orm import Session
 import models, schemas
 
-def get_news_headline(db: Session): #declaring the parameters we will query 
-    return db.query(models.News_Out).filter(models.News_Out.Headline).first()
+def get_news_headline(db: Session): 
+    return db.query(models.News_Out).first()
 
 def get_news_link(db: Session):
-    return db.query(models.News_Out).filter(models.News_Out.Link).first()
-
-def get_date_published(db: Session):
-    return db.query(models.News_Out).filter(models.News_Out.Date_Published).first()
+    return db.query(models.News_Out).first()
 
 def get_all_news(db: Session):
     return db.query(models.News_Out).all()
